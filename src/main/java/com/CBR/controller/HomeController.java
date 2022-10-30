e3c5bd805d3e186675b35223d1f2a91ba63693fd
Phan Ngọc Tuấn <phanngoctuan3001@gmail.com>
2022-10-30 23:27:12 +0700

    convert mvc

--- src/main/java/com/CBR/controller/HomeController.java
+++ src/main/java/com/CBR/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.CBR.controller;
 
+import com.CBR.service.serviceImpl.AnswerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@CrossOrigin
 public class HomeController {
 
+    @Autowired
+    AnswerServiceImpl answerServiceImpl;
     @GetMapping("/")
-    public String homePage(){
+    public String homePage(Model model){
+        model.addAttribute("answers", answerServiceImpl.findAnswerByTrongSo(6));
         return "index";
     }
 
